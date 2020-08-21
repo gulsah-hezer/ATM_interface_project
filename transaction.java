23b63815fa23cddb86f74cf633e1b68764b79b29
gulsah-hezer <66849531+gulsah-hezer@users.noreply.github.com>
2020-08-21 08:35:37 +0300

    Update transaction.java

--- transaction.java
+++ transaction.java
@@ -1,1 +1,9 @@
+import java.util.Date;
 
+public class Transaction{
+  private double amount;
+  private Date timestamp;
+  private String memo;
+  /* the account in which the transaction was performed */
+  private Account inAccount;
+}
