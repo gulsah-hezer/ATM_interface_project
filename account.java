92ebb3d2a39da9db33b64eaddeab9943b4dce19a
gulsah-hezer <66849531+gulsah-hezer@users.noreply.github.com>
2020-08-21 08:31:52 +0300

    Update account.java

--- account.java
+++ account.java
@@ -1,1 +1,10 @@
+import java.util.ArrayList;
 
+public class Account {
+  
+  private String name;
+  private double balance;
+  private String uuid;
+  private User holder;
+  private ArrayList<transaction> transactions;
+  
